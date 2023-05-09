c265b7b6fbd396c373f10ee8d5a0f07a8b8416ea
Viacheslav Sitnik <slava2261812@gmail.com>
2023-05-09 16:36:56 +0300

    add query client

---
+++ src/react-query/constants.js
@@ -0,0 +1,3 @@
+export const queryKeys = {
+  marketPrices: "market-prices",
+};
---
+++ src/react-query/queryClient.js
@@ -0,0 +1,22 @@
+import { QueryCache, QueryClient } from "react-query";
+import { toast } from "react-toastify";
+
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      refetchOnmount: false,
+      refetchOnReconnect: false,
+      retry: false,
+      staleTime: 5 * 60 * 1000,
+      enabled: false,
+    },
+  },
+  queryCache: new QueryCache({
+    onError: (e) => {
+      toast.error(e.message || `Something went wrong`, {
+        position: "bottom-left",
+      });
+    },
+  }),
+});
